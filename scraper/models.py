aac992b869987f08a08173054ca4ee78da96bc27
Morozov Ivan <moritcsh@gmail.com>
2020-03-06 01:07:21 +0500

    MVP))000))0)))

--- scraper/models.py
+++ scraper/models.py
@@ -3,12 +3,19 @@ from django.db import models
 
 class Ingredients(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255, blank=False)
+    name = models.CharField(max_length=255, blank=False, unique=True)
     measure = models.CharField(max_length=100, blank=True)
 
 
 class Meals(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255, blank=False)
-    type = models.CharField(max_length=255, blank=True)
-    ingredients = models.ManyToManyField(Ingredients)
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    tags = models.CharField(max_length=255, blank=True)
+
+
+class Ingredients_Meals(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    meals_id = models.ForeignKey(to=Meals, on_delete=models.CASCADE)
+    ingredients_id = models.ForeignKey(to=Ingredients, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=1, max_digits=5, blank=True)
+    additional_info = models.CharField(max_length=255, blank=True)
--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,13 @@
 import re
+import json
+import time
+
+from django.db.utils import IntegrityError
 
 from bs4 import BeautifulSoup
 import requests
+
+from scraper.models import Meals, Ingredients, Ingredients_Meals
 
 
 class Scraper(object):
@@ -9,15 +15,49 @@ class Scraper(object):
         self.target = 'https://eda.ru/recepty?'
 
     def perform_scraping(self):
-        # for counter in range(1, 310):
-        content = requests.get(f'{self.target}' + f'page={1}')
-        data_for_parsing = BeautifulSoup(content.text, 'lxml')
-        meals_for_parsing = data_for_parsing.find_all('h3', class_='horizontal-tile__item-title item-title')
-        for header in meals_for_parsing:
-            result = header.span.contents
-            meal_string, = result
-        ingredients_for_parsing = data_for_parsing.find_all('div', class_='ingredients-list__content')
-        for div in ingredients_for_parsing:
-            result = div.span.contents
-            ingredients = result
-            print(ingredients)
+        for counter in range(1, 310):
+            time.sleep(1)
+            content = requests.get(f'{self.target}' + f'page={counter}')
+            data_for_parsing = BeautifulSoup(content.text, 'lxml')
+            meals_for_parsing = data_for_parsing.find_all('div', class_='horizontal-tile__content')
+            for div in meals_for_parsing:
+                result = div.find('h3', class_='horizontal-tile__item-title item-title')
+                result = result.span.contents
+                meal_string, = result
+                meal_string = meal_string.strip()
+                tags_for_parsing = div.find_all('ul', class_='breadcrumbs')
+                for tag in tags_for_parsing:
+                    tags_result = tag.find_all('span')
+                    categories = re.findall(r'(?<=<span>).*?(?=</span>)', str(tags_result))
+                    categories_to_str = ''
+                    for category in categories:
+                        categories_to_str += category + ', '
+                    meal = Meals(name=meal_string, tags=categories_to_str)
+                    try:
+                        meal.save()
+                    except IntegrityError:
+                        pass
+                div_content = div.find_all('p', 'ingredients-list__content-item content-item js-cart-ingredients')
+                for p_tag in div_content:
+                    parsed_dict = json.loads(p_tag['data-ingredient-object'])
+                    regexp = re.compile(r"(?<=\s)\D+")
+                    measurement_units = re.search(regexp, parsed_dict['amount'])
+                    if measurement_units is not None:
+                        ingredients = Ingredients(name=parsed_dict['name'],
+                                                  measure=measurement_units.group(0))
+                        try:
+                            ingredients.save()
+                        except IntegrityError:
+                            pass
+                    else:
+                        ingredients = Ingredients(name=parsed_dict['name'])
+                        try:
+                            ingredients.save()
+                        except IntegrityError:
+                            pass
+                    # TODO: dodelai, psina
+                    # found_amount = re.match(r'(\d+(,\d+)?)', parsed_dict['amount'])
+                    # if found_amount:
+                    #     found_amount.group(0)
+                    # else:
+                    #     ingredients =
